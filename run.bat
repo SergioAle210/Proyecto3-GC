112f1666452bebaa45ba2b3e862e14bc3634c87b
SergioAle210 <ore221122@uvg.edu.gt>
2024-10-03 19:23:44 -0600

    FEAT: Se agregó el .bat para automaticar el cargo run --release

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\sergi\OneDrive - Universidad del Valle de Guatemala\Archivos Semestres\Tercer año - Segundo Semestre\Gráficas por Computadora\Proyecto3-GC"
+cargo run --release
+pause
