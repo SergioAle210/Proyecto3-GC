200adc3b6f4cc53d3038d8106e829f65c73d7318
SergioAle210 <ore221122@uvg.edu.gt>
2024-10-10 20:58:31 -0600

    FEAT: Se implementaron las diferentes matrices en la transformada y también para la visualización en la pantalla

--- src/shaders.rs
+++ src/shaders.rs
@@ -5,39 +5,42 @@ use nalgebra_glm::{Mat3, Vec3, Vec4};
 pub fn vertex_shader(vertex: &Vertex, uniforms: &Uniforms) -> Vertex {
     // Transform position
     let position = Vec4::new(vertex.position.x, vertex.position.y, vertex.position.z, 1.0);
-    let transformed = uniforms.model_matrix * position;
+    let transformed =
+        uniforms.projection_matrix * uniforms.view_matrix * uniforms.model_matrix * position;
 
     // Perform perspective division
     let w = transformed.w;
-    let transformed_position = Vec3::new(transformed.x / w, transformed.y / w, transformed.z / w);
+    let transformed_position =
+        Vec4::new(transformed.x / w, transformed.y / w, transformed.z / w, 1.0);
 
-    /*
-       let model_mat3 = Mat3::new(
-           uniforms.model_matrix[0],
-           uniforms.model_matrix[1],
-           uniforms.model_matrix[2],
-           uniforms.model_matrix[4],
-           uniforms.model_matrix[5],
-           uniforms.model_matrix[6],
-           uniforms.model_matrix[8],
-           uniforms.model_matrix[9],
-           uniforms.model_matrix[10],
-       );
+    let screen_position = uniforms.viewport_matrix * transformed_position;
 
-       let normal_matrix = model_mat3
-           .transpose()
-           .try_inverse()
-           .unwrap_or(Mat3::identity());
+    let model_mat3 = Mat3::new(
+        uniforms.model_matrix[0],
+        uniforms.model_matrix[1],
+        uniforms.model_matrix[2],
+        uniforms.model_matrix[4],
+        uniforms.model_matrix[5],
+        uniforms.model_matrix[6],
+        uniforms.model_matrix[8],
+        uniforms.model_matrix[9],
+        uniforms.model_matrix[10],
+    );
 
-       let transformed_normal = normal_matrix * vertex.normal;
-    */
+    let normal_matrix = model_mat3
+        .transpose()
+        .try_inverse()
+        .unwrap_or(Mat3::identity());
+
+    let transformed_normal = normal_matrix * vertex.normal;
+
     // Create a new Vertex with transformed attributes
     Vertex {
         position: vertex.position,
         normal: vertex.normal,
         tex_coords: vertex.tex_coords,
         color: vertex.color,
-        transformed_position,
-        transformed_normal: vertex.normal,
+        transformed_position: Vec3::new(screen_position.x, screen_position.y, screen_position.z),
+        transformed_normal,
     }
 }
