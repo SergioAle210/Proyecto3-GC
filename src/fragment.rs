1fed2057fe6aebac7a3d89626d53e4434aecad79
SergioAle210 <ore221122@uvg.edu.gt>
2024-11-09 11:27:57 -0600

    FEAT: Se mejoró el fragment shader

--- src/fragment.rs
+++ src/fragment.rs
@@ -1,19 +1,31 @@
 use crate::color::Color;
-use nalgebra_glm::Vec2;
+use nalgebra_glm::{Vec2, Vec3};
 
-// Pueden haber otros campos en la estructura Fragment, pero estos son los mínimos requeridos.
 pub struct Fragment {
     pub position: Vec2,
     pub color: Color,
     pub depth: f32,
+    pub normal: Vec3,
+    pub intensity: f32,
+    pub vertex_position: Vec3,
 }
 
 impl Fragment {
-    pub fn new(x: f32, y: f32, color: Color, depth: f32) -> Self {
+    pub fn new(
+        position: Vec2,
+        color: Color,
+        depth: f32,
+        normal: Vec3,
+        intensity: f32,
+        vertex_position: Vec3,
+    ) -> Self {
         Fragment {
-            position: Vec2::new(x, y),
+            position,
             color,
             depth,
+            normal,
+            intensity,
+            vertex_position,
         }
     }
 }
